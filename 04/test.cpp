40bdd01f669091aeac4a74ab152e528a6304d695
kabir <kabir.zhong@qq.com>
2016-07-27 01:03:20 +0800

    UTF.

--- 04/test.cpp
+++ 04/test.cpp
@@ -7,10 +7,16 @@ JNIEXPORT void JNICALL Java_Hello_test
   (JNIEnv *env, jobject obj)
 {
 	jclass clazz_date = env->FindClass("java/util/Date");
+
 	jmethodID mid_date = env->GetMethodID(clazz_date, "<init>", "()V");
 	jobject date = env->NewObject(clazz_date, mid_date);
-	jmethodID get_time =  env->GetMethodID(clazz_date, "getTime", "()J");
-	jlong time = env->CallLongMethod(date, get_time);
-	cout << (time+500)/1000 <<endl;
+
+	jmethodID time_id =  env->GetMethodID(clazz_date, "toString", "()Ljava/lang/String;");
+	jstring time = (jstring)env->CallObjectMethod(date, time_id);
+	
+	const char* times = env->GetStringUTFChars(time, NULL);
+	cout << times << endl;
+	env->ReleaseStringUTFChars(time, times);
 }
+
 }
